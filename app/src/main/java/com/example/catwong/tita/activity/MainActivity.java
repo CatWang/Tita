f4cd075fb294a6477e2288a4bf6cb67748ff8f18
王大喵Paper <4336228@qq.com>
2017-09-23 16:25:46 -0500

    layouts

--- app/src/main/java/com/example/catwong/tita/activity/MainActivity.java
+++ app/src/main/java/com/example/catwong/tita/activity/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.catwong.tita.R;
+import com.example.catwong.tita.common.CommonKey;
+import com.example.catwong.tita.util.PreferencesManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final PreferencesManager preferenceManager = PreferencesManager.getInstance(this);
 
         Thread loadingThread = new Thread() {
 
@@ -23,14 +26,24 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception e) {
 
                 } finally {
-                    Intent i = new Intent(MainActivity.this,
-                            HomeActivity.class);
-                    startActivity(i);
+
+                    if (preferenceManager.getBoolean(CommonKey.PREF_IS_FIRST_LAUNCH, true)) {
+                        preferenceManager.setBoolean(CommonKey.PREF_IS_FIRST_LAUNCH, false);
+                        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                    } else if (!preferenceManager.getBoolean(CommonKey.LOGGEDIN, false)) {
+                        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                    } else if (preferenceManager.getBoolean(CommonKey.LOGGEDIN, false)) {
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                    }
+
+
                     finish();
                 }
             }
         };
 
         loadingThread.start();
+
     }
 }
+
