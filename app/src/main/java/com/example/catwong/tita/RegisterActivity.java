ec54861faa0e8aedc00a23c98a5d393d58c97b34
王大喵Paper <4336228@qq.com>
2017-09-23 11:35:31 -0500

    NEW
    This reverts commit 389e510cf33b39d9be63332d41589f7e3e346d62.

--- app/src/main/java/com/example/catwong/tita/RegisterActivity.java
+++ app/src/main/java/com/example/catwong/tita/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.catwong.tita;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -10,9 +12,20 @@ import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity{
 
+    private Button registerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerButton = (Button) findViewById(R.id.register_button);
+        Intent i = new Intent(RegisterActivity.this, CalendarActivity.class);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
     }
 }
