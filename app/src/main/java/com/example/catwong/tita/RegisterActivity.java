389e510cf33b39d9be63332d41589f7e3e346d62
王大喵Paper <4336228@qq.com>
2017-09-23 10:56:05 -0500

    Revert "Revert "33""
    This reverts commit e8c8243396ff492e923bbbe85d91343130229a5d.

--- app/src/main/java/com/example/catwong/tita/RegisterActivity.java
+++ app/src/main/java/com/example/catwong/tita/RegisterActivity.java
@@ -2,12 +2,14 @@ package com.example.catwong.tita;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 /**
  * Created by CatWong on 9/23/17.
  */
 
 public class RegisterActivity extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
